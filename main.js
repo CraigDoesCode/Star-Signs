9e9c8905b874fc91b828a75624b1f71ded42f340
Craig Norford <craig.a.norford@gmail.com>
2021-05-09 20:07:49 +0100

    practice data and logic

--- main.js
+++ main.js
@@ -4,87 +4,99 @@ const starSigns = {
         endDate: new Date(0000, 3, 19), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
-
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]
     }, 
     taurus: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 3, 20),
+        endDate: new Date(0000, 4, 20), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] },
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ] 
+    },
     gemmini: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 4, 21),
+        endDate: new Date(0000, 5, 21), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     },
     cancer:{
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 5, 22),
+        endDate: new Date(0000, 6, 22), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ] 
     },
     leo:{
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 6, 23),
+        endDate: new Date(0000, 7, 22), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     },
     virgo: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 7, 23),
+        endDate: new Date(0000, 8, 22), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     },
     libra: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 9, 23),
+        endDate: new Date(0000, 10, 21), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     },
     scorpio: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 10, 22),
+        endDate: new Date(0000, 11, 21), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     }, 
     sagittarius: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 11, 22),
+        endDate: new Date(1971, 0, 19), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     }, 
     capricorn: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 0, 20),
+        endDate: new Date(0000, 1, 18), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     }, 
     aquarius: {
-        startDate: new Date(0000, 2, 21),
-        endDate: new Date(0000, 3, 19), 
+        startDate: new Date(0000, 1, 19),
+        endDate: new Date(0000, 2, 19), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     }, 
     pisces: {
         startDate: new Date(0000, 2, 21),
         endDate: new Date(0000, 3, 19), 
         positiveQuotes: ['pos 1', 'pos 2', 'pos3', 'pos 4', 'pos5'],
         negativeQuotes: ['neg 1', 'neg 2', 'neg 3', 'neg 4', 'neg 5'], 
-        Actions: ['punch', 'kick', 'kiss'] 
+        actions: ['punch', 'kick', 'kiss'],
+        result: ['death', 'life', 'everything in between' ]  
     }};
 // make star signs objects with with dates, - positive / negative predictions - positive / negative actions - emotacons
-
+// ******* remember issue with dec to jan *******
 
 const prompt = require('prompt-sync')();
 
@@ -100,23 +112,42 @@ let dateRegex = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/
 //console.log(dateOfBirth);
 while (dateValid === false){
     if (dateRegex.test(dateOfBirthInput)){
-        console.log('worked')
+       // console.log('worked')
         dateValid = true;
     }else{
-        console.log('\nThe date is not correctly formatted (dd/mm/yyyy)\n')
+        //console.log('\nThe date is not correctly formatted (dd/mm/yyyy)\n')
         dateOfBirthInput = prompt('Please enter your date of birth (dd/mm/yyyy):');
     }
 }
 // Turn input into Date object
 const dateParts = dateOfBirthInput.split('/');
-console.log(dateParts);
+//console.log(dateParts);
 const dateOfBirth = new Date(0000, dateParts[1]-1, dateParts[0]);
-console.log(dateOfBirth.toDateString());
-
+//console.log(dateOfBirth.toDateString());
+let phrase = "";
 for (sign in starSigns){
-    console.log(starSigns[sign].startDate)
+    //console.log(starSigns[sign].startDate)
     if (dateOfBirth > starSigns[sign].startDate && dateOfBirth < starSigns[sign].endDate) {
-        console.log(`you are a ${sign}!`);
+        
+        phrase += `You are a ${sign}!`;
+        //random generate a boolean for 
+        const isPositive = Math.random() < 0.5;
+        if (isPositive){
+            let index = Math.floor(Math.random() * starSigns[sign].positiveQuotes.length)
+            //console.log(index);
+            phrase += ` ${starSigns[sign].positiveQuotes[index]}`;
+            index = Math.floor(Math.random() * starSigns[sign].actions.length)
+            phrase += ` ${starSigns[sign].actions[index]}`;
+        } else {  
+            console.log('is negative');
+            let index = Math.floor(Math.random() * starSigns[sign].negativeQuotes.length)
+            phrase += ` ${starSigns[sign].negativeQuotes[index]}`;
+            index = Math.floor(Math.random() * starSigns[sign].actions.length)
+            phrase += ` ${starSigns[sign].actions[index]}`;
+        }
+        console.log(phrase);
+        
+        
     } 
 }
 // use date of birth to select star sign 
